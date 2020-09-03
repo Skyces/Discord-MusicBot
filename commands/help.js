5695e2410e0dd3a6d8a6f34c1002f42975ca8522
Skyces <70525909+Skyces@users.noreply.github.com>
2020-09-04 03:06:51 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,43 +1,75 @@
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
+
     info: {
+
         name: "help",
-        description: "To show all commands",
+
+        description: "Untuk menampilkan semua perintah",
+
         usage: "[command]",
-        aliases: ["commands", "help me", "pls help"]
+
+        aliases: ["commands", "h"]
+
     },
 
     run: async function(client, message, args){
+
         var allcmds = "";
 
         client.commands.forEach(cmd => {
+
             let cmdinfo = cmd.info
+
             allcmds+="``"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
+
         })
 
         let embed = new MessageEmbed()
-        .setAuthor("Commands of "+client.user.username, client.user.displayAvatarURL())
+
+        .setAuthor("Perintah dari "+client.user.username, client.user.displayAvatarURL())
+
         .setColor("BLUE")
-        .setDescription(allcmds)
-        .setFooter(`To get info of each command you can do ${client.config.prefix}help [command] | Hander by ItzCutePikachu#2006`)
+
+        .setDescription(LIST COMMANDS)
+
+        .setFooter(`Untuk mendapatkan info setiap perintah yang bisa Anda lakukan ${client.config.prefix}help [command] | BOT CREATE BY: Juna#7048`)
 
         if(!args[0])return message.channel.send(embed)
+
         else {
+
             let cmd = args[0]
+
             let command = client.commands.get(cmd)
+
             if(!command)command = client.commands.find(x => x.info.aliases.includes(cmd))
-            if(!command)return message.channel.send("Unknown Command")
+
+            if(!command)return message.channel.send("Perintah Tidak Diketahui")
+
             let commandinfo = new MessageEmbed()
+
             .setTitle("Command: "+command.info.name+" info")
+
             .setColor("YELLOW")
+
             .setDescription(`
-Name: ${command.info.name}
+
+Nama: ${command.info.name}
+
 Description: ${command.info.description}
-Usage: \`\`${client.config.prefix}${command.info.name} ${command.info.usage}\`\`
-Aliases: ${command.info.aliases.join(", ")}
+
+Gunakan: \`\`${client.config.prefix}${command.info.name } ${command.info.usage}\`\`
+
+Alias: ${command.info.aliases.join(", ")}
+
 `)
-            message.channel.send(commandinfo)
+
+            message.channel.send(command info)
+
         }
+
     }
+
 }
