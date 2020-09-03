56344edb47365625bda9ecbd6945f6bc31b157d3
Skyces <70525909+Skyces@users.noreply.github.com>
2020-09-04 04:12:11 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("?help | MUSIC", {
+    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
