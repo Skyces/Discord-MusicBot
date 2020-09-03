f0ee1b90eadeddcb14c2e63be09e6c3f14488ac5
Skyces <70525909+Skyces@users.noreply.github.com>
2020-09-04 03:57:53 +0700

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,30 +1,53 @@
 const { MessageEmbed } = require("discord.js");
+
 const sendError = require("../util/error");
 
 module.exports = {
+
   info: {
+
     name: "queue",
-    description: "To show the server songs queue",
+
+    description: "Untuk menampilkan antrian lagu server",
+
     usage: "",
+
     aliases: ["q", "list", "songlist", "song-list"],
+
   },
 
   run: async function (client, message, args) {
+
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue) return sendError("There is nothing playing in this server.", message.channel);
+
+    if (!serverQueue) return sendError("Tidak ada yang diputar di server ini.", message.channel);
 
     let queue = new MessageEmbed()
+
     .setTitle("Server Songs Queue")
+
     .setColor("BLUE")
-    .addField("Now Playing", serverQueue.songs[0].title, true)
+
+    .addField("Sedang Dimainkan", serverQueue.songs[0].title, true)
+
     .addField("Text Channel", serverQueue.textChannel, true)
+
     .addField("Voice Channel", serverQueue.voiceChannel, true)
+
     .setDescription(serverQueue.songs.map((song) => {
+
       if(song === serverQueue.songs[0])return
+
       return `**-** ${song.title}`
+
     }).join("\n"))
-    .setFooter("Currently Server Volume is "+serverQueue.volume)
-    if(serverQueue.songs.length === 1)queue.setDescription(`No songs to play next add songs by \`\`${client.config.prefix}play <song_name>\`\``)
+
+    .setFooter("Saat ini Volume Server adalah "+serverQueue.volume)
+
+    if(serverQueue.songs.length === 1)queue.setDescription(`Tidak ada lagu untuk diputar selanjutnya tambahkan lagu \`\`${client.config.prefix}play <nama lagu>\`\``)
+
     message.channel.send(queue)
+
   },
+
 };
